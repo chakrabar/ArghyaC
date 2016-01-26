e639059741334dced2db8c9792751cc5b6ab6488
chakrabar <chakrabar@live.com>
2016-01-27 00:45:20 +0530

    Chunk check-in
    don't blame for for issues

---
+++ ArghyaC/Controllers/QuickRouteController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ArghyaC.Controllers
+{
+    public class QuickRouteController : Controller
+    {
+        // GET: QuickRoute
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArghyaC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArghyaC.MvcApplication" Language="C#" %>
